b5a1c939857c61d63252edc6218483d78006ae50
ajaytidke28297 <ajaytidke28297@gmail.com>
2021-06-24 23:00:32 +0530

    Home section started added background image 😎😋

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import Layout from "./components/layout/Layout";
+import Home from "./components/home/Home";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 function App() {
-  return <Layout>Hello</Layout>;
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Layout>
+  );
 }
 
 export default App;
